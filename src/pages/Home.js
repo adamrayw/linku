0c5b47475fdb193b8a288a9c7d1fa45e6cbdb223
Adam Ray Wibowo <raywibowo68@gmail.com>
2022-01-07 00:11:33 +0700

    Remove navbar and footer component as default page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,8 @@ import {
   faTiktok,
 } from "@fortawesome/free-brands-svg-icons";
 import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 library.add(faFacebook, faTwitter);
 
@@ -16,58 +18,62 @@ export default function Home() {
     document.title = "Linku - Make your business more professional ";
   });
   return (
-    <main>
-      <section className="text-center max-w-xl mx-auto md:py-10 p-10 border-b">
-        <h1 className="text-4xl font-bold text-gray-700">LINKU</h1>
-        <p className="md:text-2xl text-lg text-gray-500 mt-4">
-          Link bisnis kamu dalam satu halaman
-        </p>
-        <Link
-          to="/create"
-          className="mt-6 inline-block font-semibold bg-blue-500 px-4 py-2 rounded-lg text-white hover:bg-blue-700 transition-all"
-          type="button"
-        >
-          Buat sekarang
-        </Link>
-      </section>
-      <section className="grid md:grid-cols-3 grid-cols-1 gap-6 md:py-10 p-10">
-        <div className="text-center shadow-md px-4 py-10 inline-block rounded-lg bg-white">
-          <h2 className="text-2xl text-gray-700 font-semibold">
-            Gunakan nama mu
-          </h2>
-          <p className="text-sm my-1 text-gray-500">
-            Gunakan nama mu agar lebih profesional.
+    <>
+      <Navbar />
+      <main>
+        <section className="text-center max-w-xl mx-auto md:py-10 p-10 border-b">
+          <h1 className="text-4xl font-bold text-gray-700">LINKU</h1>
+          <p className="md:text-2xl text-lg text-gray-500 mt-4">
+            Link bisnis kamu dalam satu halaman
           </p>
-          <div className="mt-4 border border-gray-400 inline-block px-4 py-2 rounded-xl">
-            <p>
-              linku.id/<span className="text-blue-500">yourname</span>
+          <Link
+            to="/create"
+            className="mt-6 inline-block font-semibold bg-blue-500 px-4 py-2 rounded-lg text-white hover:bg-blue-700 transition-all"
+            type="button"
+          >
+            Buat sekarang
+          </Link>
+        </section>
+        <section className="grid md:grid-cols-3 grid-cols-1 gap-6 md:py-10 p-10">
+          <div className="text-center shadow-md px-4 py-10 inline-block rounded-lg bg-white">
+            <h2 className="text-2xl text-gray-700 font-semibold">
+              Gunakan nama mu
+            </h2>
+            <p className="text-sm my-1 text-gray-500">
+              Gunakan nama mu agar lebih profesional.
+            </p>
+            <div className="mt-4 border border-gray-400 inline-block px-4 py-2 rounded-xl">
+              <p>
+                linku.id/<span className="text-blue-500">yourname</span>
+              </p>
+            </div>
+          </div>
+          <div className="text-center shadow-md px-4 py-10 inline-block rounded-lg bg-white">
+            <h2 className="text-2xl text-gray-700 font-semibold">
+              Banyak Pilihan
+            </h2>
+            <p className="text-sm my-1 text-gray-500">
+              Kami menyediakan banyak pilihan seperti tiktok, twitter, youtube
+              dll.
+            </p>
+            <div className="mt-8 flex justify-around text-lg">
+              <FontAwesomeIcon icon={faYoutube} />
+              <FontAwesomeIcon icon={faFacebook} />
+              <FontAwesomeIcon icon={faTiktok} />
+              <FontAwesomeIcon icon={faTwitter} />
+            </div>
+          </div>
+          <div className="text-center shadow-md px-4 py-10 inline-block rounded-lg bg-white">
+            <h2 className="text-2xl text-gray-700 font-semibold">
+              Bebas Customize
+            </h2>
+            <p className="text-sm my-1 text-gray-500">
+              Customize tampilan se menarik mungkin!
             </p>
           </div>
-        </div>
-        <div className="text-center shadow-md px-4 py-10 inline-block rounded-lg bg-white">
-          <h2 className="text-2xl text-gray-700 font-semibold">
-            Banyak Pilihan
-          </h2>
-          <p className="text-sm my-1 text-gray-500">
-            Kami menyediakan banyak pilihan seperti tiktok, twitter, youtube
-            dll.
-          </p>
-          <div className="mt-8 flex justify-around text-lg">
-            <FontAwesomeIcon icon={faYoutube} />
-            <FontAwesomeIcon icon={faFacebook} />
-            <FontAwesomeIcon icon={faTiktok} />
-            <FontAwesomeIcon icon={faTwitter} />
-          </div>
-        </div>
-        <div className="text-center shadow-md px-4 py-10 inline-block rounded-lg bg-white">
-          <h2 className="text-2xl text-gray-700 font-semibold">
-            Bebas Customize
-          </h2>
-          <p className="text-sm my-1 text-gray-500">
-            Customize tampilan se menarik mungkin!
-          </p>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+      <Footer />
+    </>
   );
 }
