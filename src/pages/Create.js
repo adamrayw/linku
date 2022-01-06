b5cf84bf74ffde1c50191c3cebbc76519027b6fd
Adam Ray Wibowo <raywibowo68@gmail.com>
2022-01-06 22:01:28 +0700

    Add modal success, field my link, change body request post

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,6 +2,7 @@ import React from "react";
 import { SketchPicker } from "react-color";
 
 export default function Create() {
+  const [link, setLink] = React.useState();
   const [namaBisnis, setNamaBisnis] = React.useState(
     "PT. MENCARI CINTA SEJATI"
   );
@@ -19,8 +20,9 @@ export default function Create() {
     React.useState(false);
   const [deskripsiFontSize, setDeskripsiFontSize] = React.useState("");
   const [namaBisnisFontSize, setNamaBisnisFontSize] = React.useState("");
-  const [postRequest, setPostRequest] = React.useState(false);
-  const [loading, setLoading] = React.useState(false);
+  const [postRequestFailed, setPostRequestFailed] = React.useState(false);
+  const [postRequestSucces, setPostRequestSucces] = React.useState(false);
+  const [loading, setLoading] = React.useState();
 
   function handleNamaBisnisColorPicker() {
     setNamaBisnisColorPicker(!namaBisnisColorPicker);
@@ -96,26 +98,26 @@ export default function Create() {
 
   async function handlePostDesign(e) {
     e.preventDefault();
-    setPostRequest(false);
+    setPostRequestFailed(false);
     setLoading(true);
     // POST Request using fetch with async/await
+    const formData = new FormData();
+    formData.append("link", link);
+    formData.append("image", selectedImage);
+    formData.append("nama", namaBisnis);
+    formData.append("nama_color", namaBisnisColor);
+    formData.append("nama_font_weight", namaBisnisStyleBold);
+    formData.append("nama_font_style", namaBisnisStyleItalic);
+    formData.append("nama_font_size", namaBisnisFontSize);
+    formData.append("deskripsi", deskripsi);
+    formData.append("deskripsi_color", deskripsiColor);
+    formData.append("deskripsi_font_weight", deskripsiStyleBold);
+    formData.append("deskripsi_font_style", deskripsiStyleItalic);
+    formData.append("deskripsi_font_size", deskripsiFontSize);
+
     const requestOptions = {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        link: namaBisnis,
-        image: "test",
-        nama: namaBisnis,
-        nama_color: namaBisnisColor,
-        nama_font_weight: namaBisnisStyleBold,
-        nama_font_style: namaBisnisStyleItalic,
-        nama_font_size: namaBisnisFontSize,
-        deskripsi: deskripsi,
-        deskripsi_color: deskripsiColor,
-        deskripsi_font_weight: deskripsiStyleBold,
-        deskripsi_font_style: deskripsiStyleItalic,
-        deskripsi_font_size: deskripsiFontSize,
-      }),
+      body: formData,
     };
     const response = await fetch(
       "http://127.0.0.1:8000/api/design",
@@ -124,9 +126,11 @@ export default function Create() {
 
     if (!response.ok) {
       console.log("Something error");
-      setPostRequest(true);
+      setPostRequestFailed(true);
       setLoading(false);
     } else {
+      setPostRequestFailed(false);
+      setPostRequestSucces(true);
       setLoading(false);
     }
   }
@@ -175,13 +179,37 @@ export default function Create() {
           {activeTab === "tab1" ? (
             <form>
               <div>
+                <label className="block text-sm mb-1  text-gray-400">
+                  My Link
+                </label>
+                <div class="mt-1 relative  rounded-md shadow-sm">
+                  <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <span class="text-gray-500 sm:text-sm">
+                      https://linku.id/
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    class="border focus:ring-indigo-500 text-blue-500 focus:border-indigo-500 placeholder-blue-500 block w-full pl-28 py-2 sm:text-sm border-gray-300 rounded-md"
+                    placeholder="yourname"
+                    onChange={(e) => {
+                      setLink(e.target.value);
+                    }}
+                  />
+                </div>
+              </div>
+              <div>
                 <label className="block text-sm mb-1 mt-4  text-gray-400">
                   Gambar
                 </label>
                 <input
                   type="file"
-                  name="file-gambar"
-                  className="border text-gray-500 w-full rounded p-1 ransition-all"
+                  name="gambar"
+                  className=" text-gray-500 w-full rounded p-1 ransition-all file:mr-4 file:py-2 file:px-4
+                  file:rounded-full file:border-0
+                  file:text-sm file:font-semibold
+                  file:bg-violet-50 file:text-blue-500
+                  hover:file:bg-violet-100 hover:file:cursor-pointer"
                   onChange={selectImage}
                 />
               </div>
@@ -331,12 +359,12 @@ export default function Create() {
                   Create
                 </button>
 
-                {postRequest ? (
+                {postRequestFailed ? (
                   <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                     <div className="px-6 py-4 rounded shadow-sm bg-red-500 z-50 ">
                       <button
                         onClick={() => {
-                          setPostRequest(false);
+                          setPostRequestFailed(false);
                         }}
                       >
                         <svg
@@ -373,6 +401,62 @@ export default function Create() {
                       <p className="font-light mt-1 text-sm text-white text-center">
                         Silahkan coba lagi.
                       </p>
+                    </div>
+                  </div>
+                ) : (
+                  ""
+                )}
+                {postRequestSucces ? (
+                  <div className="fixed max-w-sm top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    <div className="px-6 py-4 rounded shadow-sm bg-gray-50 z-50 ">
+                      <button
+                        onClick={() => {
+                          setPostRequestSucces(false);
+                        }}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="absolute right-0 text-blue-500 top-0 m-2 h-4 w-4 ml-auto"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                      </button>
+
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-16 w-16 mx-auto text-blue-500"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                      <p className="font-bold mt-2 text-xl text-blue-500 text-center">
+                        Berhasil Membuat!
+                      </p>
+                      <p className="font-light mt-1 text-sm text-blue-500  text-center">
+                        Di bawah adalah link kamu, silahkan copy & paste di bio
+                        social media kamu!
+                      </p>
+                      <input
+                        type="text"
+                        className="border w-full text-center mt-4 mb-2 border-blue-500 bg-white rounded-md px-4 py-2"
+                        value={`linku.id/${link}`}
+                        disabled
+                      />
                     </div>
                   </div>
                 ) : (
