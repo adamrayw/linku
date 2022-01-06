19634a04d04f386668de26420d7fe230c3b7c463
Adam Ray Wibowo <raywibowo68@gmail.com>
2022-01-06 10:06:40 +0700

    Change shadow navbar to shadow-sm

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-white rounded-b-lg shadow mb-3">
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-white rounded-b-lg shadow-sm mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a
