9c7756cda329ab4638a9b2fc27d82270744f8f61
Adam Ray Wibowo <raywibowo68@gmail.com>
2022-01-07 23:08:16 +0700

    Change component name

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
 import Create from "./pages/Create";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import View from "./pages/View";
 
 function App() {
   return (
     <div className="App max-w-4xl mx-auto">
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
         <Route path="login" element={<Login />} />
+        <Route path="/:link" element={<View />} />
       </Routes>
-      <Footer />
     </div>
   );
 }
