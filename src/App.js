b76326874cdef0a15ae20f9e5494f47e39663d90
Adam Ray Wibowo <raywibowo68@gmail.com>
2022-01-13 18:43:31 +0700

    Wrap routes with Context Provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { useState } from "react";
+import { Context } from "./context/context";
 import Create from "./pages/Create";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import View from "./pages/View";
 
 function App() {
+  const [context, setContext] = useState([]);
+
   return (
     <div className="App max-w-4xl mx-auto">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="login" element={<Login />} />
-        <Route path="/:link" element={<View />} />
-      </Routes>
+      <Context.Provider value={[context, setContext]}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="login" element={<Login />} />
+          <Route path="/:link" element={<View />} />
+        </Routes>
+      </Context.Provider>
     </div>
   );
 }
