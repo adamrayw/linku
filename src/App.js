06fa70fad6fc65a508c84637bd0f2e877a07f924
Adam Ray Wibowo <raywibowo68@gmail.com>
2022-01-16 18:34:55 +0700

    add tema provider and tema state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { useState } from "react";
-import { Context } from "./context/context";
+import { Context, Tema } from "./context/context";
 import Create from "./pages/Create";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -9,17 +9,20 @@ import View from "./pages/View";
 
 function App() {
   const [context, setContext] = useState([]);
+  const [tema, setTema] = useState("bg-gray-200");
 
   return (
     <div className="App max-w-4xl mx-auto">
-      <Context.Provider value={[context, setContext]}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="login" element={<Login />} />
-          <Route path="/:link" element={<View />} />
-        </Routes>
-      </Context.Provider>
+      <Tema.Provider value={[tema, setTema]}>
+        <Context.Provider value={[context, setContext]}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="login" element={<Login />} />
+            <Route path="/:link" element={<View />} />
+          </Routes>
+        </Context.Provider>
+      </Tema.Provider>
     </div>
   );
 }
