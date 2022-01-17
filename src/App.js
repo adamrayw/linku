57675b46c7e417d29a7deb72a4b155d7558b693b
Adam Ray Wibowo <raywibowo68@gmail.com>
2022-01-17 13:41:24 +0700

    Wrap routes with IndexBorder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { useState } from "react";
-import { Context, Tema } from "./context/context";
+import { Context, IndexBorder, Tema } from "./context/context";
 import Create from "./pages/Create";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -10,19 +10,22 @@ import View from "./pages/View";
 function App() {
   const [context, setContext] = useState([]);
   const [tema, setTema] = useState("bg-gray-200");
+  const [index, setIndex] = useState();
 
   return (
     <div className="App max-w-4xl mx-auto">
-      <Tema.Provider value={[tema, setTema]}>
-        <Context.Provider value={[context, setContext]}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/create" element={<Create />} />
-            <Route path="login" element={<Login />} />
-            <Route path="/:link" element={<View />} />
-          </Routes>
-        </Context.Provider>
-      </Tema.Provider>
+      <IndexBorder.Provider value={[index, setIndex]}>
+        <Tema.Provider value={[tema, setTema]}>
+          <Context.Provider value={[context, setContext]}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/create" element={<Create />} />
+              <Route path="login" element={<Login />} />
+              <Route path="/:link" element={<View />} />
+            </Routes>
+          </Context.Provider>
+        </Tema.Provider>
+      </IndexBorder.Provider>
     </div>
   );
 }
