015f3cea472a722c51c8a0b74130809ad7f1de75
Adam Ray Wibowo <raywibowo68@gmail.com>
2022-01-17 12:56:18 +0700

    All themes now are available to use

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -99,27 +99,135 @@ export default function Themes(props) {
           <p className="text-xs text-gray-400">Violet Fuchsia</p>
         </div>
         <div className="text-center space-y-1 flex flex-col justify-center items-center">
-          <div className="h-16 w-16 bg-gradient-to-br from-purple-500 to-pink-500 rounded"></div>
+          <button
+            value="bg-gradient-to-br from-purple-500 to-pink-500"
+            className={`h-16 w-16 bg-gradient-to-br from-purple-500 to-pink-500 rounded ${
+              clickedBorder === 4 ? "border-4 border-blue-500" : ""
+            } rounded`}
+            onClick={(e) => {
+              setClickedBorder(4);
+              setTema(e.target.value);
+            }}
+          >
+            {clickedBorder === 4 ? (
+              <FontAwesomeIcon
+                icon={faCheckCircle}
+                className="text-green-400 "
+              />
+            ) : (
+              ""
+            )}
+          </button>
           <p className="text-xs text-gray-400">Purple Pink</p>
         </div>
         <div className="text-center space-y-1 flex flex-col justify-center items-center">
-          <div className="h-16 w-16 bg-gradient-to-br from-gray-700 via-gray-900 to-black rounded"></div>
+          <button
+            value="bg-gradient-to-br from-gray-700 via-gray-900 to-black"
+            className={`h-16 w-16 bg-gradient-to-br from-gray-700 via-gray-900 to-black rounded ${
+              clickedBorder === 5 ? "border-4 border-blue-500" : ""
+            } rounded`}
+            onClick={(e) => {
+              setClickedBorder(5);
+              setTema(e.target.value);
+            }}
+          >
+            {clickedBorder === 5 ? (
+              <FontAwesomeIcon
+                icon={faCheckCircle}
+                className="text-green-400 "
+              />
+            ) : (
+              ""
+            )}
+          </button>
           <p className="text-xs text-gray-400">GOTHAM</p>
         </div>
         <div className="text-center space-y-1 flex flex-col justify-center items-center">
-          <div className="h-16 w-16 bg-gradient-to-br from-indigo-200 via-red-200 to-yellow-100 rounded"></div>
+          <button
+            value="bg-gradient-to-br from-indigo-200 via-red-200 to-yellow-100"
+            className={`h-16 w-16 bg-gradient-to-br from-indigo-200 via-red-200 to-yellow-100 rounded ${
+              clickedBorder === 6 ? "border-4 border-blue-500" : ""
+            } rounded`}
+            onClick={(e) => {
+              setClickedBorder(6);
+              setTema(e.target.value);
+            }}
+          >
+            {clickedBorder === 6 ? (
+              <FontAwesomeIcon
+                icon={faCheckCircle}
+                className="text-green-400 "
+              />
+            ) : (
+              ""
+            )}
+          </button>
           <p className="text-xs text-gray-400">SUNSET</p>
         </div>
         <div className="text-center space-y-1 flex flex-col justify-center items-center">
-          <div className="h-16 w-16 bg-gradient-to-br from-purple-200 via-purple-400 to-purple-800 rounded"></div>
+          <button
+            value="bg-gradient-to-br from-purple-200 via-purple-400 to-purple-800"
+            className={`h-16 w-16 bg-gradient-to-br from-purple-200 via-purple-400 to-purple-800 rounded ${
+              clickedBorder === 7 ? "border-4 border-blue-500" : ""
+            } rounded`}
+            onClick={(e) => {
+              setClickedBorder(7);
+              setTema(e.target.value);
+            }}
+          >
+            {clickedBorder === 7 ? (
+              <FontAwesomeIcon
+                icon={faCheckCircle}
+                className="text-green-400 "
+              />
+            ) : (
+              ""
+            )}
+          </button>
           <p className="text-xs text-gray-400">Huckleberry</p>
         </div>
         <div className="text-center space-y-1 flex flex-col justify-center items-center">
-          <div className="h-16 w-16 bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 rounded"></div>
+          <button
+            value="bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400"
+            className={`h-16 w-16 bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 rounded ${
+              clickedBorder === 8 ? "border-4 border-blue-500" : ""
+            } rounded`}
+            onClick={(e) => {
+              setClickedBorder(8);
+              setTema(e.target.value);
+            }}
+          >
+            {clickedBorder === 8 ? (
+              <FontAwesomeIcon
+                icon={faCheckCircle}
+                className="text-green-400 "
+              />
+            ) : (
+              ""
+            )}
+          </button>
           <p className="text-xs text-gray-400">POWERPUFF</p>
         </div>
         <div className="text-center space-y-1 flex flex-col justify-center items-center">
-          <div className="h-16 w-16 bg-gradient-to-br from-pink-300 via-purple-300 to-indigo-400 rounded"></div>
+          <button
+            value="bg-gradient-to-br from-pink-300 via-purple-300 to-indigo-400"
+            className={`h-16 w-16 bg-gradient-to-br from-pink-300 via-purple-300 to-indigo-400 rounded ${
+              clickedBorder === 9 ? "border-4 border-blue-500" : ""
+            } rounded`}
+            onClick={(e) => {
+              setClickedBorder(9);
+              setTema(e.target.value);
+            }}
+          >
+            {clickedBorder === 9 ? (
+              <FontAwesomeIcon
+                icon={faCheckCircle}
+                className="text-green-400 "
+              />
+            ) : (
+              ""
+            )}
+          </button>
           <p className="text-xs text-gray-400">COTTON CANDY</p>
         </div>
       </div>
