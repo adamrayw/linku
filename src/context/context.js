1bbfc77f51ee1224134af6aa0892ea1289a3f65b
Adam Ray Wibowo <raywibowo68@gmail.com>
2022-01-17 13:40:42 +0700

    Add context for indexBorder

--- src/context/context.js
+++ src/context/context.js
@@ -2,6 +2,7 @@ import { createContext } from "react";
 
 export const Context = createContext([[], () => {}]);
 export const Tema = createContext("");
+export const IndexBorder = createContext();
 
 // export const ContextProvider = ({ children }) => {
 //   const [context, setContext] = useState([]);
