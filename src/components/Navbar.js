3ab762139c16790cc1149da85d7a44b20d5d1fde
Adam Ray Wibowo <raywibowo68@gmail.com>
2022-01-19 17:59:29 +0700

    Navbar rounded while mid screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-white rounded-b-lg shadow mb-3">
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-white md:rounded-b-lg shadow mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a
@@ -42,7 +42,7 @@ export default function Navbar() {
               <li className="nav-item">
                 <a
                   className="px-3 py-2 flex items-center text-xs uppercase font-medium leading-snug text-blue-500 hover:opacity-75"
-                  href="#pablo"
+                  href="#feature"
                 >
                   <span className="ml-2">Feature</span>
                 </a>
