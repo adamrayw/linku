2aa2ea5f47774e0005602b907141ce50db129676
Adam Ray Wibowo <raywibowo68@gmail.com>
2022-01-19 17:59:50 +0700

    Change feature

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,14 @@
 import React from "react";
-import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faFacebook,
-  faTwitter,
-  faYoutube,
-  faTiktok,
-} from "@fortawesome/free-brands-svg-icons";
+
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-
-library.add(faFacebook, faTwitter);
+import {
+  faInfinity,
+  faLink,
+  faPencilRuler,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   React.useEffect(() => {
@@ -35,37 +32,41 @@ export default function Home() {
           </Link>
         </section>
 
-        <section className="grid md:grid-cols-3 grid-cols-1 gap-6 md:py-10 py-10 px-14 ">
-          <div className="text-center shadow px-4 py-10 inline-block rounded-lg bg-white">
-            <h2 className="text-2xl text-gray-700 font-semibold">
-              Gunakan nama apa saja
-            </h2>
+        <section
+          className="grid md:grid-cols-3 grid-cols-1 gap-6 md:py-10 py-10 px-14 "
+          id="#feature"
+        >
+          <div className="text-center px-4 py-10 inline-block bg-white">
+            <FontAwesomeIcon
+              icon={faLink}
+              size="2x"
+              className="text-blue-500 mb-2"
+            />
+            <h2 className="text-2xl text-gray-700 font-semibold">Your Link</h2>
             <p className="text-sm my-2 text-gray-500">
               Gunakan nama personal, brand atau perusahaan agar semakin mudah
-              dikenal
+              dikenal.
             </p>
-            <div className="mt-4 border border-gray-400 inline-block px-4 py-2 rounded-xl">
-              <p>
-                /<span className="text-blue-500">yourname</span>
-              </p>
-            </div>
           </div>
-          <div className="text-center shadow px-4 py-10 inline-block rounded-lg bg-white">
+          <div className="text-center px-4 py-10 inline-block bg-white">
+            <FontAwesomeIcon
+              icon={faInfinity}
+              size="2x"
+              className="text-blue-500 mb-2"
+            />
             <h2 className="text-2xl text-gray-700 font-semibold">
-              Banyak Pilihan Logo
+              Tak Terbatas
             </h2>
             <p className="text-sm my-2 text-gray-500">
-              Kami menyediakan banyak pilihan logo terkenal seperti tiktok,
-              twitter, youtube dll.
+              Buat link atau halaman tak terbatas.
             </p>
-            <div className="mt-8 flex justify-around text-lg">
-              <FontAwesomeIcon icon={faYoutube} />
-              <FontAwesomeIcon icon={faFacebook} />
-              <FontAwesomeIcon icon={faTiktok} />
-              <FontAwesomeIcon icon={faTwitter} />
-            </div>
           </div>
-          <div className="text-center shadow px-4 py-10 inline-block rounded-lg bg-white">
+          <div className="text-center px-4 py-10 inline-block bg-white">
+            <FontAwesomeIcon
+              icon={faPencilRuler}
+              size="2x"
+              className="text-blue-500 mb-2"
+            />
             <h2 className="text-2xl text-gray-700 font-semibold">
               Bebas Customize
             </h2>
